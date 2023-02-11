8a1dc0d954292604ed3c27d18f0be1630c01949b
jiye <jym6853@gmail.com>
2023-02-11 18:42:18 +0900

    component create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+function Container() {
+  return <div id="container">container</div>;
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div>footer</div>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Container></Container>
+      <Footer></Footer>
     </div>
   );
 }
