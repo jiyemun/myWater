9da13efd218f7325fe161a789c5b4c8889dc8db8
jiye <jym6853@gmail.com>
2023-02-11 19:11:14 +0900

    props test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import "./App.css";
 
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>header</h1>
+      <h1>{props.title}</h1>
     </header>
   );
 }
 
 function Container() {
-  return <div id="container">container</div>;
+  let list = [
+    { id: 1, text: "사과" },
+    { id: 2, text: "당근" },
+    { id: 3, text: "수박" },
+  ];
+  return (
+    <div id="container">
+      <Article item={list} />
+    </div>
+  );
+}
+
+function Article({ item }) {
+  let el = [];
+  for (let i = 0; i < item.length; i++) {
+    el.push(<div key={item[i].id}>{item[i].text}</div>);
+  }
+  return <article>{el}</article>;
 }
 
 function Footer() {
@@ -23,7 +40,7 @@ function Footer() {
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header title="my header"></Header>
       <Container></Container>
       <Footer></Footer>
     </div>
